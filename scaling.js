219685d0415db6ca86b80bb8dc01e25c46a56a52
Martin <sokoma@email.cz>
2020-11-17 21:11:16 +0100

    fixed scalling

--- scaling.js
+++ scaling.js
@@ -1,6 +1,8 @@
 const gameBodyWrapper = document.getElementById("game-body-wrapper");
 const timerWrapper = document.getElementById("timer-wrapper");
 
-gameBodyWrapper.style.width = screen.width + "px";
-gameBodyWrapper.style.height = screen.width + "px";
-timerWrapper.style.width = screen.width + "px";
+const screenWidthPlusPadding = screen.width - 15;
+
+gameBodyWrapper.style.width = screenWidthPlusPadding + "px";
+gameBodyWrapper.style.height = screenWidthPlusPadding + "px";
+timerWrapper.style.width = screenWidthPlusPadding + "px";
